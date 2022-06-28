a08328af0a765f61a5a33bc3c196c8379275a5f7
Valtteri <valtteri.ak@gmail.com>
2022-06-28 14:20:14 +0300

    2.4 is done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -24,38 +24,76 @@ const Content = ( { parts } ) => {
   )
 }
 
-const Course = ( { course } ) => {
+const Total = ( { parts } ) => {
+	let total = 0
+	parts.map(part => total += part.exercises)
 	return (
 	    <div>
-		  <Header name={course.name} />
-		  <Content parts={course.parts} />
+	      <strong>Total of {total} exercises</strong>
+	    </div>
+	)
+}
+
+const Courses = ( { courses } ) => {
+	return (
+	    <div>
+		{courses.map(course =>
+			<>
+			<Header name={course.name}/>
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+			</>
+			)}
 	    </div>
 	)
 }
 
 const App = () => {
-  const course = {
-	  id: 1,
-	name: 'Half Stack application development',
-	parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-	      id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-	      id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-	      id: 3
-      }
-    ]
-  }
-  return <Course course={course} />
+const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return <Courses courses={courses} />
 }
 
 export default App
